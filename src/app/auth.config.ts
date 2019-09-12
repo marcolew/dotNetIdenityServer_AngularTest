5df7fe3980cb90b6694f7caf1c2c959faff00ebd
Marco Lewandowski <lewandowski@swms.de>
2019-09-12 18:00:54 +0200

    working login

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -2,15 +2,21 @@ import { AuthConfig } from "angular-oauth2-oidc";
 
 export const authConfig: AuthConfig = {
   // Url of the Identity Provider
-  issuer: "https://steyer-identity-server.azurewebsites.net/identity",
+  issuer: "http://localhost:5000",
+
+  requireHttps: false,
+
+  dummyClientSecret: "secret",
+
+  responseType: "code id_token",
 
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + "/index.html",
 
   // The SPA's id. The SPA is registered with this id at the auth-server
-  clientId: "spa-demo",
+  clientId: "mvc",
 
   // set the scope for the permissions the client should request
   // The first three are defined by OIDC. The 4th is a usecase-specific one
-  scope: "openid profile email voucher"
+  scope: "openid profile api1 offline_access"
 };
